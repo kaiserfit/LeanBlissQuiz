9318f44f733cdd722b3a984980883fe13620c436
KaiserFit <service@kaiserfit.net>
2024-02-15 02:49:45 +0800

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,17 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
+// Redirect from root to /purple-honey
+app.get('/', (req, res) => {
+  res.redirect(301, '/purple-honey.html');
+});
+
+// Route for /purple-honey
+app.get('/purple-honey.html', (req, res) => {
+  // Your logic for rendering the content for /purple-honey goes here
+  res.send('This is the content for /purple-honey');
+});
+
 // Set up the 'public' directory to serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 
